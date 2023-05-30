3469f5130068b1107c8702747bdceea835c46f89
Aleksandr Liubenkov <lubenkov.a@gmail.com>
2023-05-30 17:40:55 +0300

    Hardhat compile in docker file, fix compiler download

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,11 +1,5 @@
 #!/bin/sh
 # Change to the correct directory
 cd /usr/src/app;
-# Run hardhat
-npx hardhat run
-# Keep node alive
-set -e
-if [ "${1#-}" != "${1}" ] || [ -z "$(command -v "${1}")" ]; then
-  set -- node "$@"
-fi
-exec "$@"
+# Run hardhat tests
+npm run deploy
