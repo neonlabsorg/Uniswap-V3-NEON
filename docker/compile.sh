46e3f10cedc977e2c7c86757472827fdea4c296c
Aleksandr Liubenkov <lubenkov.a@gmail.com>
2023-05-31 11:10:02 +0300

    Add sleep between compile attempts

--- docker/compile.sh
+++ docker/compile.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
+# Number of attempts to compile artifacts
 COUNT=10
+# How long to wait between unlucky attempts
+WAIT=10
+# Expected directory to appear after successful compilation
 DIR="artifacts"
 
 cd /usr/src/app
@@ -13,6 +17,11 @@ do
   if [ -d "$DIR" ]; then
     echo "Artifacts compiled"
     break
+  fi
+
+  if [ ! "$i" -eq $COUNT ]; then
+    echo "Unlucky attempt, sleep" $WAIT "seconds"
+    sleep $WAIT
   fi
 done
 
